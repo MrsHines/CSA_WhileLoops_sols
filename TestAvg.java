becca7d7c02d5629f8c7491609b4d1b660bb4869
Emily Hines <34042849+MrsHines@users.noreply.github.com>
2020-11-05 14:58:06 -0600

    Update and rename TestCounter.java to TestAvg.java

--- TestAvg.java
+++ TestAvg.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class TestCounter
+public class TestAvg
 {
   public static void main(String args[]) {
     TestCounter test = new TestCounter();
